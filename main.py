693bcfd42a63f8d46d804ee2cb68003ff5e2b387
Gary Homewood <gary.homewood@gmail.com>
2015-04-03 19:02:20 +0100

    refactoring after source html change

--- main.py
+++ main.py
@@ -6,45 +6,39 @@ from flask import Flask
 from flask import render_template
 from flask import jsonify
 
-page = requests.get('http://dictionary.reference.com/wordoftheday')
-tree = html.fromstring(page.text)
+wordHtml = html.fromstring(requests.get('http://dictionary.reference.com/wordoftheday').text)
+word = (wordHtml.xpath('(//*[@class="wotd-wrapper wotd-requested wotd-today"]//@data-word)'))[0]
+definitions = (wordHtml.xpath('(//*[@class="definition-box"]/ol)[1]/li'))
+usages = wordHtml.xpath('//*[@data-word="' + word + '"]/div[2]/div[2]/div/blockquote/span')
+origin = wordHtml.cssselect('div.origin-content')[0].text_content()
 
-# extract content from the page
-word = (tree.cssselect('h2.me'))[0].text
-pronounciation = "\\" + (tree.xpath("//*[@id='wotdcont']/div[1]/div[1]/span[@class='show_spellpr']/span[@class='pron']"))[0].text_content()
-wordType = tree.xpath("//*[@id='wotdcont']/div[1]/div[1]/span[@class='show_spellpr']/span[@class='pron pos']")[0].text_content().replace(";", "")
-definitions = tree.xpath("//*[@id='wotdcont']/div[1]/div[@class='defns rr_wid']")
-quotes = tree.cssselect('div.quote')
-sources = tree.cssselect('div.au_src')
-origin = (tree.xpath('//*[@id="wotdcont"]/div/div/div[@class="origin"]'))[0].text_content()
+wordDetailHtml = html.fromstring(requests.get('http://dictionary.reference.com/browse/' + word).text)
+pronounciation = wordDetailHtml.cssselect('span.pron.spellpron')[0].text_content()
+wordType = wordDetailHtml.cssselect('div.def-list > section > header')[0].text_content()
 
 # populate a dictionary with the word definitions, keyed on word type (noun, verb, etc)
 definitionDictionary = {}
 definitionList = []
+for definition in definitions:
+	definitionList.append(definition.text_content())
 
-for definitionLine in definitions[0]:
-	# check for noun/verb indicator
-	if definitionLine.get('class') == "pron pos" and definitionLine.text_content():
-		# add list of definitions to dictionary and reset to build another list
-		definitionDictionary[wordType] = definitionList
-		wordType = definitionLine.text_content().replace(":", "")
-		definitionList = []
-
-	# get the definitions and add them to the list
-	for definitionElement in definitionLine:
-		if definitionElement.get('class') == "defn":
-			definitionList.append(definitionElement.text_content())
-
-# add remaining list of definitions to dictionary
 definitionDictionary[wordType] = definitionList
 
 # populate a dictionary of quotes
+quotes = []
+sources = []
+for usage in usages:
+	if usage.attrib.get("class") == None:
+		quotes.append(usage.text_content())
+	elif usage.attrib.get("class") == "author":
+		sources.append(usage.text_content())
+
 quoteList = []
 i = 0
 for quote in quotes:
 	q = {
-		'quote': quote.text_content(),
-		'source': sources[i].text_content().replace("--", "")
+		'quote': quote,
+		'source': sources[i]
 			}
 	quoteList.append(q)
 	i = i + 1
